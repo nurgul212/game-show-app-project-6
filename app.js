72805b526584a5781166430f5abbeb858d26b1a1
Nurgul Amat <nurgulamat212@gmail.com>
2021-11-02 21:30:35 -0600

    add app.js

---
+++ app.js
@@ -0,0 +1,53 @@
+// console.log('hello hello');
+// Get the element with the id/className of qwerty, phrase, overlay and save them to variables.
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const startButton = document.querySelector('.btn__reset');
+const overlay = document.getElementById('overlay');
+
+// Listen for the start game button to be pressed
+startButton.addEventListener('click', () => {
+    overlay.style.display = 'none';
+});
+
+// Create an array named phrases
+const phrases = [
+ 'stop uyghur geonocide',
+ 'no pain no gain',
+ 'time flies',
+ 'you matter',
+ 'life is short',
+];
+
+
+// Return a random phrase from an array
+const getRandomPhraseAsArray = arr => {
+
+
+}
+
+// Add the letters of a string to the display
+const addPhraseToDisplay = arr => {
+
+
+}
+
+// Check if a letter is in the phrase
+const checkLetter = button => {
+
+
+}
+
+// Check if the game has been won or lost
+const checkWin = () => {
+
+
+}
+
+
+
+// Listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
+
+
+});
